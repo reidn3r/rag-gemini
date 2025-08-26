37b77cfa4571908f092a1a658c94b01656ae2c07
reidn3r <rdn.adn00@gmail.com>
2025-08-25 23:21:39 -0300

    fix: splitter correto

--- src/services/rag/ingestion/ingestion_service.py
+++ src/services/rag/ingestion/ingestion_service.py
@@ -2,13 +2,11 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../..")))
 
 import asyncio
-from typing import List
 from pathlib import Path
 from pypdf import PdfReader
 from src.config.bootstrap import bootstrap
 from src.services.llm.gemini import GeminiLLM
-from langchain.docstore.document import Document
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import  RecursiveCharacterTextSplitter
 from src.repository.embedding_repository import EmbeddingRepository
 from src.services.preprocessing.text_preprocessing_service import Preprocessing
 
@@ -28,28 +26,23 @@ async def ingest_pdf():
   repository = EmbeddingRepository()
 
   for filename in os.listdir(DOCUMENTS_DIR):
-    texts: List[str] = []
     if filename.lower().endswith(".pdf"):
       path = os.path.join(DOCUMENTS_DIR, filename)
       buffer = read_pdf(path)
-      
       clean_buffer = " ".join(preprocessor_wrapper.run([buffer]))
-      texts.append(clean_buffer)
-    
-    splitter = CharacterTextSplitter(chunk_size=64, chunk_overlap=12)
-    docs: List[Document] = []
-    
-    for text in texts:
-      chunks = splitter.split_text(text)
-      for c in chunks:
-        docs.append(Document(page_content=c))
-
-    for doc in docs:
-      vector = await llm_wrapper.embed(doc.page_content)
-      n_tokens = llm_wrapper.count_tokens(doc.page_content)
-      await repository.save(filename, doc.page_content, n_tokens, vector.embeddings[0].values)
+      
+      splitter = RecursiveCharacterTextSplitter(
+        chunk_size=256,
+        chunk_overlap=64,
+        separators=["\n\n", "\n", " ", ""]
+      )
+      chunks = splitter.split_text(clean_buffer)      
+      for chunk in chunks:
+        vector = await llm_wrapper.embed(chunk)
+        n_tokens = llm_wrapper.count_tokens(chunk)
+        repository.save(filename, chunk, n_tokens, vector.embeddings[0].values)
   
-  await repository.adjust_ivfflat_index()
+  repository.adjust_ivfflat_index()
 
 if __name__ == "__main__":
     bootstrap()
